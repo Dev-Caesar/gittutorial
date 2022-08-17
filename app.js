a4b36a1ed12f1b5ead625a2e144d861b87eb8956
Nweke Caesar <caesarridd@gmail.com>
2022-08-17 16:44:41 +0100

    Revert "removed the john object"
    This reverts commit 5840108189813ef0136a87fee22be552d31f2606.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 let num = 20;
 let num2 = 20 + 10;
+<<<<<<< HEAD
 let str = `lets see how this goes`;
 let array = [2, 3, 4, 5, 6];
+let obj = { name: 'John', age: 40 };
 let newObj = { name: 'Alice' };
-
-
+=======
+let str = `lets see how this goes`;
+>>>>>>> parent of 17ef4d4 (added an array)
